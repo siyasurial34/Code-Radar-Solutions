784f1718ae3d61fc741985f38cf64131b2246802
Siya Surial <siyasurial34@gmail.com>
2025-04-01 12:41:12 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,14 +1,25 @@
-int fibonacciSeries(int n){
-    static int n1=0,n2=1,n3;
-    if(n==1){
-        return 0;
-    }
-    
-    if(n>0){
-      n3=n1+n2;
-      printf("%d",n3);
-      n1=n2;
-      n2=n3;
-      fibonacciSeries(n-1);
+void fib(int n) {
+    static int n1 = 0, n2 = 1;  // Static variables to maintain state
+    if (n > 0) {
+        if (n == 1) {  
+            printf("0\n");  // Special case for n = 1
+            return;
+        }
+
+        if (n == 2) {
+            printf("0 1\n");  // Special case for n = 2
+            return;
+        }
+
+        printf("0 1 ");  // Print the first two Fibonacci numbers
+
+        int n3;  
+        for (int i = 2; i < n; i++) {
+            n3 = n1 + n2;
+            printf("%d ", n3);
+            n1 = n2;
+            n2 = n3;
+        }
+        printf("\n");
     }
 }
